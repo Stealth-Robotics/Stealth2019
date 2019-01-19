b5e89b955dcb2dea2b4ba6668d28acf29518b583
4089Software <stealthrobotics@gmail.com>
2019-01-18 17:20:49 -0800

    more formatting

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -13,23 +13,24 @@ package frc.robot;
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
-public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
-
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
-
-  public static final int driveLF = 1;
-  public static final int driveLR = 2;
-  public static final int driveRF = 3;
-  public static final int driveRR = 4;
-
-  public static final int elevator = 5;
-
-  public static final int pigeonIMU = driveLF;
+public class RobotMap 
+{
+    // For example to map the left and right motors, you could define the
+    // following variables to use with your drivetrain subsystem.
+    // public static int leftMotor = 1;
+    // public static int rightMotor = 2;
+  
+    // If you are using multiple modules, make sure to define both the port
+    // number and the module. For example you with a rangefinder:
+    // public static int rangefinderPort = 1;
+    // public static int rangefinderModule = 1;
+  
+    public static final int driveLF = 1;
+    public static final int driveLR = 2;
+    public static final int driveRF = 3;
+    public static final int driveRR = 4;
+  
+    public static final int elevator = 5;
+  
+    public static final int pigeonIMU = driveLF;
 }
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -9,26 +9,30 @@ import edu.wpi.first.cameraserver.*;
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
  */
-public class Vision extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
-
-  public Vision() {
-    new Thread(() -> {
-      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-      camera.setResolution(640, 480);
-      camera.setFPS(25);
-    }).start();
-
-    SmartDashboard.putBoolean("Vision/Initialized", true);
-
-    SmartDashboard.putNumber("Vision/TestNum", 10.25);
-  }
-
-
-  @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
-  }
+public class Vision extends Subsystem 
+{
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+  
+    public Vision() 
+    {
+        new Thread(() -> 
+        {
+            UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+            camera.setResolution(640, 480);
+            camera.setFPS(25);
+        }).start();
+    
+        SmartDashboard.putBoolean("Vision/Initialized", true);
+    
+        SmartDashboard.putNumber("Vision/TestNum", 10.25);
+    }
+  
+  
+    @Override
+    public void initDefaultCommand() 
+    {
+        // Set the default command for a subsystem here.
+        // setDefaultCommand(new MySpecialCommand());
+    }
 }
