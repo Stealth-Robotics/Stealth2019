0f561afe0c5cc7289543c9d478968b9743b68f7b
4089Software <stealthrobotics@gmail.com>
2019-01-18 16:58:31 -0800

    random stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,10 +1,15 @@
 
 package frc.robot;
 
+/**
+ * Holds the constants in one place
+ */
 public class Constants
 {
+    //constants for the drive axes
     public static final int DRIVE_JOYSTICK_Y = 1;
     public static final int DRIVE_JOYSTICK_X = 2;
 
+    //twist axis
     public static final int DRIVE_JOYSTICK_TWIST = 0;
 }
